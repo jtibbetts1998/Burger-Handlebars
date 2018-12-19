3ae58cb0217201a44da3eb9252d5719b7fc83f6d
jtibbetts1998 <justin.tibbetts98@gmail.com>
2018-12-19 13:53:32 -0500

    controller logic and tables complete

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,9 @@
-CREATE burgers_db
-NAME 
+CREATE DATABASE burgers_db
+USE burgers_db;
+
+CREATE TABLE burgers {
+    id INT NOT NUll AUTO_INCREMENT NOT NULL,
+    'burger_name' VARCHAR(255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY(id)
+};
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whooper', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', FALSE);

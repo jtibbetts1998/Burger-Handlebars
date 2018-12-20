cae88630e18b6a205bc2f2d4c53da36ed114f3be
jtibbetts1998 <justin.tibbetts98@gmail.com>
2018-12-20 10:52:40 -0500

    establishing tables in dbeaver correction

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ USE burgers_db;
 
 CREATE TABLE burgers (
     id INT NOT NUll AUTO_INCREMENT NOT NULL,
-    'burger_name' VARCHAR(50) NOT NULL,
+    burger_name VARCHAR(50) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY(id)
 );

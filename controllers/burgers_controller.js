4eca83ffce1faa75f53d22cdb30555c745479a53
jtibbetts1998 <justin.tibbetts98@gmail.com>
2018-12-20 12:39:09 -0500

    adding html to main handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,20 +17,20 @@ router.post("/api/burgers", function(req, res) {
         "burger_name", "devoured"
     ], [
         req.body.name, req.body.devoured
-    ], function(result) {
-        res.json({ id: result.insertId });
+    ], function(res) {
+        res.json({ id: res.insertId });
     });
 });
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
 
     console.log("condition", condition);
 
     burger.update({
         devoured: req.body.devoured
-    }, condition, function(result) {
-     if (result.changedRows == 0) {
+    }, condition, function(res) {
+     if (res.changedRows == 0) {
          return res.status(404).end();
     } else {
         res.status(200).end();
@@ -41,8 +41,8 @@ router.put("/api/cats/:id", function(req, res) {
 router.delete("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.param.id;
 
-    burger.delete(condition, function(result) {
-        if (result.affectedRows == 0) {
+    burger.delete(condition, function(res) {
+        if (res.affectedRows == 0) {
             return res.status(404).end();
         } else {
             res.status(200).end();
